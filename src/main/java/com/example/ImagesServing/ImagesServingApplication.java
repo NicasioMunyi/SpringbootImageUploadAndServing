6ee09e4150303b124e26ba597d703e67888c9a1b
NicasioMunyi <nickalfred200@gmail.com>
2024-04-13 17:09:32 +0300

    initial commit

---
+++ src/main/java/com/example/ImagesServing/ImagesServingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImagesServing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesServingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagesServingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImagesServing/ImagesServingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImagesServing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesServingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
